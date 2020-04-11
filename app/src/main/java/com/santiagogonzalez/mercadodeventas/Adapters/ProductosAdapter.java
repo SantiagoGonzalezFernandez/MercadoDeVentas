da260a4569b16c472e0e6a8f67886c9c7d5b6c0e
Santiago Gonzalez Fernandez <santiagogonzalezfernandezpro@gmail.com>
2020-04-11 16:18:11 -0300

    Termino la descripcion del producto y tambien agrego un listener al recycler view para poder ir a la descripcion del producto

--- app/src/main/java/com/santiagogonzalez/mercadodeventas/Adapters/ProductosAdapter.java
+++ app/src/main/java/com/santiagogonzalez/mercadodeventas/Adapters/ProductosAdapter.java
@@ -1,10 +1,15 @@
 package com.santiagogonzalez.mercadodeventas.Adapters;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,14 +22,36 @@ import java.util.ArrayList;
 public class ProductosAdapter extends RecyclerView.Adapter<ProductosAdapter.ProductoViewHolder> {
 
     ArrayList<ProductoInfo> myArrayListDeProductos;
+    Dialog myDialog;
 
     public ProductosAdapter(ArrayList<ProductoInfo> myArrayListDeProductos){
         this.myArrayListDeProductos = myArrayListDeProductos;
     }
 
     public ProductoViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,null,false);
-       return new ProductoViewHolder(view);
+       final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,null,false);
+       final ProductoViewHolder myProductoViewHolder = new ProductoViewHolder(view);
+
+       myDialog = new Dialog(view.getContext());
+       myDialog.setContentView(R.layout.fragment_description_product);
+
+       myProductoViewHolder.myLinearLayoutCeldaDelProducto.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               TextView myTextViewNombreDelProducto = myDialog.findViewById(R.id.DescriptionProductFragment_TextView_NombreDelProducto);
+               TextView myTextViewPrecioDelProducto = myDialog.findViewById(R.id.DescriptionProductFragment_TextView_PrecioDelProducto);
+               ImageView myImageViewImagenDelProducto = myDialog.findViewById(R.id.DescriptionProductFragment_ImageView_ImagenDelProducto);
+
+               myTextViewNombreDelProducto.setText(myArrayListDeProductos.get(myProductoViewHolder.getAdapterPosition()).getMyStringNombreDelProducto());
+               myTextViewPrecioDelProducto.setText(myArrayListDeProductos.get(myProductoViewHolder.getAdapterPosition()).getMyStringPrecioDelProducto());
+               myImageViewImagenDelProducto.setImageResource(myArrayListDeProductos.get(myProductoViewHolder.getAdapterPosition()).getMyIntImagenDelProducto());
+
+               Toast.makeText(view.getContext(), "Test Click " + String.valueOf(myProductoViewHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+               myDialog.show();
+           }
+       });
+
+       return myProductoViewHolder;
     }
 
     public void onBindViewHolder(ProductoViewHolder holder, int position){
@@ -38,12 +65,16 @@ public class ProductosAdapter extends RecyclerView.Adapter<ProductosAdapter.Prod
     }
 
     public class ProductoViewHolder extends RecyclerView.ViewHolder{
+
+        LinearLayout myLinearLayoutCeldaDelProducto;
         TextView myTextViewNombreDelProducto;
         TextView myTextViewPrecioDelProducto;
         ImageView myImageViewImagenDelProducto;
 
         public ProductoViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            myLinearLayoutCeldaDelProducto = itemView.findViewById(R.id.ItemList_LinearLayout_Parent);
             myTextViewNombreDelProducto = itemView.findViewById(R.id.ItemList_TextView_NombreDelProducto);
             myTextViewPrecioDelProducto = itemView.findViewById(R.id.ItemList_TextView_PrecioDelProducto);
             myImageViewImagenDelProducto = itemView.findViewById(R.id.ItemList_ImageView_ImagenDelProducto);
---
+++ app/src/main/java/com/santiagogonzalez/mercadodeventas/Fragments/DescriptionProductFragment.java
@@ -0,0 +1,32 @@
+package com.santiagogonzalez.mercadodeventas.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.santiagogonzalez.mercadodeventas.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DescriptionProductFragment extends Fragment {
+
+
+    public DescriptionProductFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description_product, container, false);
+    }
+
+}
