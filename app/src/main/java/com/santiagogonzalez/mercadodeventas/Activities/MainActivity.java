c77d642b5347481409ad685ca13a6735f65dad5d
Santiago Gonzalez Fernandez <santiagogonzalezfernandezpro@gmail.com>
2020-04-11 11:21:19 -0300

    Creo un sistema de navegacion a partir del Navegation View y genero un nuevo fragment que va ser llenado por el about Us

--- app/src/main/java/com/santiagogonzalez/mercadodeventas/Activities/MainActivity.java
+++ app/src/main/java/com/santiagogonzalez/mercadodeventas/Activities/MainActivity.java
@@ -1,29 +1,42 @@
 package com.santiagogonzalez.mercadodeventas.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
 import com.santiagogonzalez.mercadodeventas.Fragments.ListaProductosFragment;
 import com.santiagogonzalez.mercadodeventas.R;
 
-public class MainActivity extends AppCompatActivity implements ListaProductosFragment.OnFragmentInteractionListener{
-
-    ListaProductosFragment myListaProductoFragment;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myListaProductoFragment = new ListaProductosFragment();
+        final DrawerLayout myDrawerLayout = findViewById(R.id.MainActivity_DrawerLayout_Parent);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.MainActivity_FrameLayout_ContenedorDeFragments, myListaProductoFragment).commit();
+        findViewById(R.id.MainActivity_ImageView_Menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView myNavigationView = findViewById(R.id.MainActivity_NavigationView_NavegadorDePantallas);
+        myNavigationView.setItemIconTintList(null);
+
+        NavController myNavController = Navigation.findNavController(this,R.id.MainActivity_fragment_navHostFragment);
+        NavigationUI.setupWithNavController(myNavigationView, myNavController);
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
